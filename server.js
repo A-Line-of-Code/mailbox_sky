f571ec59b78f5175ab51fdcc6c58b95e5495ec0e
s <hyerim.kwon@it.weltec.ac.nz>
2020-07-25 13:31:05 +1200

    using database

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//const fs = require("fs");
+const fs = require("fs");
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -7,17 +7,15 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//const data = fs.readFileSync("./database.json");
-//const conf = JSON.parse(data);
-//const mysql = require("mysql");
-//const { config } = require("process");
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
 
-/*
 const connection = mysql.createConnection({
   host : conf.host,
   user: conf.user,
   password: conf.password,
-  port: conf.password,
+  port: conf.port,
   database: conf.database
 });
 
@@ -25,28 +23,11 @@ connection.connect();
 
 app.get('/api/tech', (req, res) => {
   connection.query(
-    "SELECT name,img,msg FROM TECH;",
+    "SELECT * FROM TECH;",
     (err, rows, fields) => {
       res.send(rows);
     }
   );
-});
-*/
-
-
-app.get('/api/tech', (req, res) => {
- res.send([
-  {
-    "name":"1", 
-    "img" : "1",
-    "msg" : "1"
-},
-{
-    "name":"11", 
-    "img" : "11",
-    "msg" : "11"
-}
- ])
 });
 
 app.post('/api/world', (req, res) => {
