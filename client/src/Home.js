698aeb1038d1c23d86e3dd1a2ed6541e58a23976
s <hyerim.kwon@it.weltec.ac.nz>
2020-07-28 17:08:13 +1200

    installed axios

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,17 +1,89 @@
 import React, {useState} from "react";
 import {useHistory} from "react-router-dom";
+import axios from "axios";
 import './App.css';
 
 export const Home = () => {
 
-  
   const [to, setTo] = useState("")
   const [password, setPassword] = useState("")
   const history = useHistory()
 
-  const submit = () => {     
-    try {
-      fetch("/api/read", {            
+
+  
+  
+  const read = () => {history.push("/read") }
+  const error = () => {alert("Incorrect.");}
+
+/*
+  const submit = () => {
+    return new Promise((resolve,reject)=>{
+    fetch("/api/read", {            
+        method: "POST", 
+        mode: 'cors',     
+        headers: { 
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({ 
+          to: to, 
+          password: password               
+      })        
+    }).then(res => console.log(res)) 
+  }) 
+  }
+*/
+/*
+
+const submit = e => {
+  e. preventDefault()
+ add()
+  
+}
+
+  const add = () =>{
+   axios({
+      method: "post",
+      url: "/api/read",
+      data:{
+        to: to, 
+        password: password             
+    }
+    })
+    .then((response) => { 
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+*/
+/*
+  const test=() => {
+    e.preventDefault()
+    submit()
+        .then((response) => {
+            console.log(response.data);
+        })
+  };
+  
+
+    const submit = async () => {     
+      addCustomer = () => {
+        const url = '/api/read';
+        const formData = new FormData();
+        formData.append('image', this.state.file);
+        formData.append('name', this.state.userName);
+        formData.append('birthday', this.state.birthday);
+        formData.append('gender', this.state.gender);
+        formData.append('job', this.state.job);
+        const config = {
+            headers: {
+              "Content-type": "application/json;'
+            }
+        }
+        return post(url, formData, config);
+    }
+      const response = await fetch("/api/read", {            
         method: "POST",          
         body: JSON.stringify({ 
             to: to, 
@@ -20,24 +92,38 @@ export const Home = () => {
         headers: { 
             "Content-type": "application/json; charset=UTF-8"
         } 
-      }) 
-     /* if(response.text()==="error"){
-        alert("incorrect info");
-      }
-      else{*/
-        history.push("/read")
-      /*  }*/
+      });
       
-    } catch (e) {
-      alert(e.message);
-    }
+      return response.body
+      
   }
-  
-  const about = () =>{history.push("/about")}
-    
+  */
+
+const submit = e=> {     
+  e.preventDefault()
+    fetch("/api/read", {            
+      method: "POST",          
+      body: JSON.stringify({ 
+          to: to, 
+          password: password               
+      }),             
+      headers: { 
+          "Content-type": "application/json; charset=UTF-8"
+      } 
+    })
+    .then((response) => {
+            console.log(response.data);
+        })
+}
+
+
+const about = () =>{history.push("/about")}
+
+
     return(
-      
+             
         <div className="App">
+        <img src="img/mail1.jpg" width="10%" alt="mail"/>
         <h1 className="app-title">Mail Box</h1>
         <form className="App-sender" onSubmit={submit}>
                    
--- server.js
+++ server.js
@@ -5,13 +5,19 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//var jsonParser = bodyParser.json()
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.use(session({secret: 'ssshhhhh'}));
 
 const data = fs.readFileSync("./database.json");
 const conf = JSON.parse(data);
 const mysql = require("mysql");
+const { response } = require("express");
 var sess;
 
 const connection = mysql.createConnection({
@@ -23,31 +29,38 @@ const connection = mysql.createConnection({
 });
 
 connection.connect();
-
+/*
+app.post('/login', jsonParser, function (req, res) {
+  console.log(req.body);
+  res.send('welcome, ' + req.body.to)
+})
+*/
 app.post('/api/read', function(req, res) {
+ 
   sess=req.session;
-  var to = req.body.to; 
-  var pw= req.body.password;
+  const to = req.body.to; 
+  const pw= req.body.password;
   console.log("server"+to+pw);
-  
+  console.log(req.body);
+ 
 	connection.query(
-    'SELECT * FROM letter WHERE receiver = ? AND password = ?', [to, pw],
-    (err, rows, fields) => {      
+    'SELECT * FROM letter WHERE receiver = ? AND password = ?', [to, pw],    
+    (err, rows, fields) => {        
+      
       if (rows.length > 0) {
         sess.receiver = to;
-        sess.password = pw;  
-        console.log("length>0");
-                 			
+        sess.password = pw; 
+        console.log(rows); 
+       return res.status(200).send("true");                     
       }
-      else {
+      else{      
         console.log("else"); 
         sess.receiver = "error";
-        sess.password = "error";               
-      }	
-      res.end();		
-    }
-  );
-  	
+        sess.password = "error";
+        return res.status(400).send("false");              
+      }      
+});
+
 });
 
 app.get('/api/letter', (req, res) => {
@@ -63,13 +76,6 @@ app.get('/api/letter', (req, res) => {
   );
 });
 
-/*
-app.get('/api/tech', (req, res) => {
-  connection.query(
-    "SELECT * FROM TECH;",
-    (err, rows, fields) => { res.send(rows);}
-  );
-});
-*/
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
