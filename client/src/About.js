7b0e4b1be3fd5138d803ae7e92ffcc40bfa90919
s <hyerim.kwon@it.weltec.ac.nz>
2020-07-29 22:15:03 +1200

    commit6

--- client/src/About.js
+++ client/src/About.js
@@ -8,12 +8,12 @@ export const About = () => {
             <div className="about">
             <div className="tech">
               <p className="blue">Development</p>
-              <div className="title"> React, Node.js, Fetch API, <br /> MySQl (Amazon RDS)</div>
-              
+              <div className="title"> React, Node.js, Fetch API, MySQl (Amazon RDS)</div>
+              <img src="/img/react.jpg" alt="React" height="300px" />
+                <img src="/img/node.jpg" alt="Node.js" height="300px" /><br/>
               <p className="blue">Deployment</p>
               <div className="title">              
               <a rel="GitHub" href="https://github.com/A-Line-of-Code/MailBox.git" target="_blank">Docker deployment files</a><br/>
-              
               </div>
             </div>
             </div>
