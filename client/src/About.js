9cf7d308af8fda73f11cc6d66a77857a4c0faefe
s <hyerim.kwon@it.weltec.ac.nz>
2020-08-08 12:42:04 +1200

    book edit4

--- client/src/About.js
+++ client/src/About.js
@@ -8,11 +8,12 @@ export const About = () => {
             <div className="about">
             <div className="tech">
               <p className="blue">Development</p>
-              <div className="tech"> React, Node.js, Fetch API, MySQl (Amazon RDS)</div><br />
+              <div className="tech"> React, Node.js, Fetch API <br /> MySQl (Amazon RDS), Amazon S3</div><br />
               <img src="/img/react.jpg" alt="React" height="300px" />
                 <img src="/img/node.jpg" alt="Node.js" height="300px" /><br/><br/>              
               <div className="tech">              
-              <a rel="GitHub" href="https://github.com/A-Line-of-Code/MailBox.git" target="_blank">GitHub</a><br/>
+              <a rel="GitHub" href="https://github.com/A-Line-of-Code/MailBox.git" target="_blank">GitHub</a> &nbsp;&nbsp;
+              <a rel="LinkedIn" href="https://www.linkedin.com/in/hyerim-sky-kwon-8787b0147/" target="_blank">LinkedIn</a><br/>
               </div>
             </div>
             </div>
